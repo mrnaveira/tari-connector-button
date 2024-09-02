7830ff94b91f71ed625a887b21e94b50ab1437ce
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2024-09-02 11:08:59 +0100

    react is null problem

--- src/TariConnectButton.tsx
+++ src/TariConnectButton.tsx
@@ -3,19 +3,19 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import TariLogoWhite from "./content/tari-logo-white.svg";
 import { TariWalletSelectionDialog } from "./TariWalletSelectionDialog";
-import useTariProvider from "./store/provider";
 import { TariProvider } from "@tari-project/tarijs";
 
 interface Props {
   onConnected?: (provider: TariProvider) => void;
+  getProvider: () => TariProvider | null;
+  setProvider: (provider: TariProvider) => void;
   signalingServerUrl: string;
   snapId: string;
   walletConnectProjectId: string | null;
 }
 
 export function TariConnectButton(props: Props) {
-  const { provider, setProvider } = useTariProvider();
-  const { onConnected, signalingServerUrl, snapId, walletConnectProjectId } = props;
+  const { onConnected, getProvider, setProvider, signalingServerUrl, snapId, walletConnectProjectId } = props;
   const [walletSelectionOpen, setWalletSelectionOpen] = React.useState(false);
 
   const handleConnectClick = () => {
@@ -27,7 +27,7 @@ export function TariConnectButton(props: Props) {
   };
 
   const handleOnConnected = (provider: TariProvider) => {
-    setProvider(provider);
+    setProvider(provider)
     onConnected?.(provider);
   };
 
@@ -35,7 +35,7 @@ export function TariConnectButton(props: Props) {
     <>
       <Button variant="contained" onClick={handleConnectClick}>
         <img width="30px" height="30px" src={TariLogoWhite} />
-        <div style={{ paddingLeft: "10px" }}>{provider?.isConnected() ? "Connected" : "Connect"}</div>
+        <div style={{ paddingLeft: "10px" }}>{getProvider()?.isConnected() ? "Connected" : "Connect"}</div>
       </Button>
       <TariWalletSelectionDialog
         open={walletSelectionOpen}
---
+++ vite.config.ts
@@ -0,0 +1,62 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import dts from 'vite-plugin-dts'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      },
+    },
+    //Generates sourcemaps for the built files,
+    //aiding in debugging.
+    sourcemap: true,
+    //Clears the output directory before building.
+    emptyOutDir: true,
+  },
+  plugins: [react(), dts()],
+  resolve: {
+    // These are polyfills to get @metamask/provider to work in the browser.
+    // https://gist.github.com/FbN/0e651105937c8000f10fefdf9ec9af3d
+    alias: {
+      stream: "rollup-plugin-node-polyfills/polyfills/stream",
+      //events: "rollup-plugin-node-polyfills/polyfills/events",
+      util: "rollup-plugin-node-polyfills/polyfills/util",
+      buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
+      process: "rollup-plugin-node-polyfills/polyfills/process-es6",
+    },
+    // This option is needed for local development
+    // https://github.com/vitejs/vite/issues/15412
+    preserveSymlinks: true,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        //NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+});
